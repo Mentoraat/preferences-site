55c585fde7c676733e76444e077d158358f08c5c
Tim van der Lippe <timlinks@hotmail.com>
2015-04-18 14:30:50 +0200

    Closes #12.
    Added bootstrap to resources folder. Must copy bootstrap.min.css to css folder in order to be used in the header.

--- application/views/header/index.php
+++ application/views/header/index.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php linkCSS('vendor/bootstrap.min'); ?>
 		<?php linkCSS('site'); ?>
 		<?php linkCSS($this->router->class); ?>
     <title>Mentoraat</title>
